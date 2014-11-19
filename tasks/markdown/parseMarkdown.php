02c25c87c8289ef62e54cdade755a19ef061f327
Matti van de Weem <mattiweem@hotmail.com>
2014-11-19 09:24:14 +0100

    Better markdown task
    Give it a theme!

--- tasks/markdown/parseMarkdown.php
+++ tasks/markdown/parseMarkdown.php
@@ -3,8 +3,12 @@ include_once('parsedown.php');
 $parsedown = new Parsedown;
 
 $path = realpath('');
+$theme = '';
+if(isset($t->theme) && $t->theme != ''):
+	$theme = file_get_contents(realpath().'/'.$t->theme);
+endif;
 foreach(glob($path.'/'.$t->watch) as $file):
-	file_put_contents($path.'/'.$t->output.str_replace('.md','',basename($file)).'.html',$parsedown->text(file_get_contents($file)));
+	file_put_contents($path.'/'.$t->output.str_replace('.md','',basename($file)).'.html',$parsedown->text(file_get_contents($file)).$theme);
 endforeach;
 echo 'Markdown parsed ('.$t->output.')'."\n";
 ?>
