c59f2d62e893d2519a9b72b6c525c7b591148f98
Matti van de Weem <mattiweem@hotmail.com>
2014-11-19 09:25:12 +0100

    fixes the path

--- tasks/markdown/parseMarkdown.php
+++ tasks/markdown/parseMarkdown.php
@@ -5,7 +5,7 @@ $parsedown = new Parsedown;
 $path = realpath('');
 $theme = '';
 if(isset($t->theme) && $t->theme != ''):
-	$theme = file_get_contents(realpath().'/'.$t->theme);
+	$theme = file_get_contents('tasks/markdown/theme/'.$t->theme);
 endif;
 foreach(glob($path.'/'.$t->watch) as $file):
 	file_put_contents($path.'/'.$t->output.str_replace('.md','',basename($file)).'.html',$parsedown->text(file_get_contents($file)).$theme);
